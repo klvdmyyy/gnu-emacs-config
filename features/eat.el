11d56e3c2a0e7c3341983153690357e303e8868b
Dmitry <klementievd08@yandex.ru>
2025-06-04 19:15:40 +0500

    eat or project-eat on `s-E`

--- features/eat.el
+++ features/eat.el
@@ -6,14 +6,22 @@
 
 (use-package eat
   :commands (eat eshell)
-  :bind ("s-E" . eat)
+  :bind ("s-E" . eat-project-or-eat)
   :hook ((eshell-load . eat-eshell-mode)
 	 (eshell-load . eat-eshell-visual-command-mode))
   :custom
+  (eat-shell (let ((zsh (executable-find "zsh")))
+	       (if zsh zsh (executable-find "bash"))))
   (eat-line-input-ring-size 1024)
   (eat-kill-buffer-on-exit t)
   (eat-term-scrollback-size nil)
   (eat-enable-mouse t))
+
+(defun eat-project-or-eat (&optional arg)
+  (interactive "P")
+  (if (project-current)
+      (eat-project arg)
+    (eat)))
 
 (provide 'features/eat)
 
