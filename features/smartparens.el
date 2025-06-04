54008a473386c89efe6f6838540607abd1818736
Dmitry <klementievd08@yandex.ru>
2025-06-04 18:30:25 +0500

    Ignore "'" and "`" in lisp/scheme

--- features/smartparens.el
+++ features/smartparens.el
@@ -6,11 +6,20 @@
 
 (use-package smartparens
   :hook ((prog-mode . smartparens-mode)
+	 ;; Strict mode only in lisp/scheme
 	 (emacs-lisp-mode . smartparens-strict-mode)
 	 (lisp-mode . smartparens-strict-mode)
 	 (common-lisp-mode . smartparens-strict-mode)
 	 (scheme-mode . smartparens-strict-mode))
   :config
+  ;; Ignore "'" and "`" in lisp/scheme mode
+  (dolist (mode '(emacs-lisp-mode
+		  lisp-mode
+		  common-lisp-mode
+		  scheme-mode))
+    (sp-local-pair mode "'" nil :when '(sp-in-string-p))
+    (sp-local-pair mode "`" nil :when '(sp-in-string-p)))
+
   (show-paren-mode 1))
 
 (use-package smartparens-config
