1eef7bc00ef33dbdef7cd37a5c8f6748b00595ae
Dmitry <klementievd08@yandex.ru>
2025-06-04 19:47:16 +0500

    Remove another GNU Emacs garbage

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((dired-mode
-  . ((dired-omit-files . "transient\\|projects\\|customs.el\\|eln-cache\\|elpaca\\'\\|recentf\\|eshell\\'\\|auto-save-list\\'")
+  . ((dired-omit-files . "tramp\\|transient\\|projects\\|customs.el\\|eln-cache\\|elpaca\\'\\|recentf\\|eshell\\'\\|auto-save-list\\'")
      )
   ))
