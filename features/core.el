dce8434c8b6fb69b5344e76db4c15e79dd4b0ada
Dmitry <klementievd08@yandex.ru>
2025-06-04 23:05:44 +0500

    Added line highlighting

--- features/core.el
+++ features/core.el
@@ -52,6 +52,7 @@
 	(message "Emacs ready in %.2f seconds with %d garbage collections"
 		 (float-time (time-subtract after-init-time before-init-time))
 		 gcs-done)))
+   (prog-mode . hl-line-mode)
    )
 
   :custom
