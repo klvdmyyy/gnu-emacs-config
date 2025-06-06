a39cb877a0fce8068da3db8ec9926b79b26590c9
Dmitry <klementievd08@yandex.ru>
2025-06-06 22:59:54 +0500

    Basic agenda setup (dummy)

--- features/productivity.el
+++ features/productivity.el
@@ -8,9 +8,16 @@
   :ensure nil
   :hook ((org-mode . org-indent-mode))
   :bind (:map
-	 org-mode-map
-	 ;; or `consult-outline'
-	 ("C-s" . consult-org-heading)))
+	     org-mode-map
+	     ;; or `consult-outline'
+	     ("C-s" . consult-org-heading)))
+
+(use-package org-agenda
+  :ensure nil
+  :commands (org-agenda)
+  :custom
+  (org-directory "~/org")
+  (org-agenda-files (directory-files-recursively org-directory "\\.agenda.org\\'")))
 
 (provide 'features/productivity)
 
