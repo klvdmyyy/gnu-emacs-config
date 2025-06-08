5d5177db147627e4daf076db8df8a782c875d777
Dmitry <klementievd08@yandex.ru>
2025-06-08 17:48:54 +0500

    A little org configuration changes. More laterly =)

--- features/productivity.el
+++ features/productivity.el
@@ -2,22 +2,32 @@
 ;;
 ;;; Commentary:
 ;;
+;; Org related things which boost my productivity.
+;;
 ;;; Code:
 
 (use-package org
   :ensure nil
   :hook ((org-mode . org-indent-mode))
-  :bind (:map
-	     org-mode-map
+  :bind (("C-c o t s" . org-timer-start)
+         ("C-c o t e" . org-timer-stop)
+         ("C-c o t p" . org-timer-pause-or-continue)
+         :map org-mode-map
 	     ;; or `consult-outline'
-	     ("C-s" . consult-org-heading)))
+	     ("C-s" . consult-org-heading))
+  :custom
+  (org-directory "~/org"))
 
 (use-package org-agenda
   :ensure nil
+  :bind (("C-c o a" . org-agenda))
   :commands (org-agenda)
   :custom
-  (org-directory "~/org")
-  (org-agenda-files (directory-files-recursively org-directory "\\.agenda.org\\'")))
+  (org-agenda-files
+   (directory-files-recursively
+    org-directory
+    ;; *.agenda.org
+    "\\.agenda.org\\'")))
 
 (provide 'features/productivity)
 
