5584e18a64340aa27e7932bf6e376934281bfe58
Dmitry <klementievd08@yandex.ru>
2025-06-14 02:33:27 +0500

    Some keybindings for `telega.el`

--- features/telega.el
+++ features/telega.el
@@ -7,6 +7,11 @@
 (use-package telega
   :commands (telega)
   :hook ((telega-chat-mode . toggle-input-method))
+  :bind (:map
+         telega-chat-mode-map
+         ("C-s" . telega-chatbuf-filter-search)
+         ("M-g <" . telega-chatbuf-history-beginning)
+         ("M-g >" . telega-chatbuf-read-all))
   :config
   (setq telega-company-backends (cons 'company-yasnippet telega-company-backends)))
 
