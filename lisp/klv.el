47894f7c95d77c5324799eaeabfad456a1d20f53
Dmitry <klementievd08@yandex.ru>
2025-06-13 19:50:07 +0500

    Add unnecessary function =)

--- lisp/klv.el
+++ lisp/klv.el
@@ -102,6 +102,20 @@
   (maybe-install-font klv/default-font
                       klv/default-font-src))
 
+(defun which-linux-distribution ()
+  "Maybe useful for future development of my GNU Emacs configuration.
+
+I don't use it now.
+
+[IMPORTANT] You need a `lsb_release' executable. Without it function doesn't
+work and just return Unknown."
+  (interactive)
+  (when (eq system-type 'gnu/linux)
+    (let* ((lsb_release (executable-find "lsb_release"))
+           (cmd (if lsb_release (concat lsb_release " -sd") nil))
+           (result (if cmd (shell-command-to-string cmd) "Unknown")))
+      (message result))))
+
 ;; Macroses
 
 (defmacro load-feature (name)
