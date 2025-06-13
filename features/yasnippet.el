70a6bdad005126695e4928b39026753a37a4a735
Dmitry <klementievd08@yandex.ru>
2025-06-14 01:26:47 +0500

    Trying to configure `telega.el` with snippets and company

--- features/yasnippet.el
+++ features/yasnippet.el
@@ -8,7 +8,8 @@
   :init
   (setq yas-snippet-dirs (seq-map #'file-truename klv/snippets))
   :hook ((prog-mode . yas-minor-mode)
-         (org-mode . yas-minor-mode))
+         (org-mode . yas-minor-mode)
+         (telega-chat-mode . yas-minor-mode))
   :config
   (yas-reload-all 1))
 
