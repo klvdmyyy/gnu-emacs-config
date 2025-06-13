4c9e7fb9cb2420b2c3ccb3d2c64d4b556a022620
Dmitry <klementievd08@yandex.ru>
2025-06-13 22:47:05 +0500

    Add so little company-idle-delay (can cause performance issues)

--- features/company.el
+++ features/company.el
@@ -22,7 +22,7 @@
          company-backends))
   :custom
   (company-dabbrev-downcase 0)
-  (company-idle-delay 0.0) ;; or 0.001/0.01 (default is 0.2)
+  (company-idle-delay 0.001) ;; or 0.001/0.01 (default is 0.2)
   (company-minimum-prefix-length 2)
   (ess-r--no-company-meta t))
 
