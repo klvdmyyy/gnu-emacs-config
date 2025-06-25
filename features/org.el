87598ff8e3c8300e2f9dba9f23ddf600961c0d26
Dmitry <klementievd08@yandex.ru>
2025-06-25 18:52:48 +0500

    Use tree-sitter in org-mode

--- features/org.el
+++ features/org.el
@@ -20,6 +20,22 @@
          :map org-mode-map
          ("C-s" . consult-org-heading)  ; MAYBE `consult-outline'
          )
+  :config
+  (setq org-src-lang-modes
+        (append
+         (seq-map (lambda (al)
+                    ;; Use Tree Sitter for C and C++ in Org Mode Source Blocks
+                    (cond
+                     ((or (string-equal (car al) "C")
+                          (string-equal (car al) "c"))
+                      '("C" . c-ts))
+                     ((or (string-equal (car al) "C++")
+                          (string-equal (car al) "cpp"))
+                      '("C++" . c++-ts))
+                     (t al)))
+                  org-src-lang-modes)
+         ;; TODO Maybe move it to languages/go.el configuration
+         '(("go" . go-ts) ("Go" . go-ts) ("golang" . go-ts) ("Golang" . go-ts))))
   :custom
   (org-directory "~/org")
   (org-capture-templates
