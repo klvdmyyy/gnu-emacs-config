af2abbe43fd1814abc8ab99c1a835afa5b59428e
Dmitry <klementievd08@yandex.ru>
2025-07-02 21:10:42 +0500

    Some changes

--- bin/emacs-timer-poll.sh
+++ bin/emacs-timer-poll.sh
@@ -2,7 +2,5 @@
 
 # Use this script if you need to display
 # Emacs Org timer in your bar. (eww, waybar, etc)
-#
-# It's also don't display timer in Emacs mode-line/frame-title
 
-emacsclient -e "(progn (setq org-timer-display nil) (org-timer-value-string))"
+emacsclient -e "(org-timer-value-string)"
