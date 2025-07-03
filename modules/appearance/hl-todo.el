59aae7b921192ba3ca1a3dae0489b2577e7a80ed
Dmitry <klementievd08@yandex.ru>
2025-07-03 19:00:44 +0500

    Some changes

--- modules/appearance/hl-todo.el
+++ modules/appearance/hl-todo.el
@@ -5,6 +5,9 @@
 ;;; Code:
 
 (use-package hl-todo
-  :hook ((prog-mode . hl-todo-mode)))
+  :hook ((prog-mode . hl-todo-mode))
+  :custom
+  (hl-todo-require-punctuation t)
+  (hl-todo-highlight-punctuation ":"))
 
 ;;; hl-todo.el ends here
