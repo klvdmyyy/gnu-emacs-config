f411d6c21d071c7142dd76159fd27d46ca8e9857
Dmitry <klementievd08@yandex.ru>
2025-08-17 16:11:16 +0500

    test.el file with testing features for future configuration

---
+++ test.el
@@ -0,0 +1,30 @@
+;;; test.el --- ASd
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(defmacro feature! (name &rest body)
+  "Provide BODY by feature NAME."
+  (declare (indent defun))
+  (let ((fvn (intern (concat "enable-" (prin1-to-string (eval `(,@name))) "-feature"))))
+    `(progn
+       (defvar ,fvn
+         "Enable this features.")
+
+       (when ,fvn
+         ,@body))))
+
+(defmacro enable-feature! (&rest plist)
+  "Enable PLIST features."
+  nil)
+
+(macroexpand
+ '(feature! 'ide
+    (eglot-ensure)))
+
+(enable-feature! 'ide)
+
+(provide 'test)
+
+;;; test.el ends here
